98cf124f990012aa1972cc607c756bde899f9be8
Abdelrahman El Kinani <kinani95@outlook.com>
2018-12-14 12:12:38 +0000

    Add projcts and corresponding images

---
+++ PersonalWebsite/Controllers/ProjectsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PersonalWebsite.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DXEvents()
+        {
+            return View();
+        }
+        public IActionResult SQ88()
+        {
+            return View();
+        }
+        public IActionResult Time()
+        {
+            return View();
+        }
+        public IActionResult Confess()
+        {
+            return View();
+        }
+        public IActionResult FanarCore()
+        {
+            return View();
+        }
+    }
+}
